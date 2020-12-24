9216c50f9ec458949031fe10bad6a12089750d25
Adrian Ilisei <adrian.ilisei@hanna.ro>
2020-12-24 20:52:15 +0200

    Test controller behavior

--- src/Controller.java
+++ src/Controller.java
@@ -18,34 +18,40 @@ public class Controller {
         return controller;
     }
 
-    public Boolean saveUser() {
+    public boolean saveUser() {
         try {
             FileOutputStream file = new FileOutputStream(model.getUserName());
             ObjectOutputStream out = new ObjectOutputStream(file);
             out.writeObject(model);
             out.close();
             file.close();
-            return Boolean.TRUE;
+            return true;
         } catch (IOException e) {
-            return Boolean.FALSE;
+            return false;
         }
     }
 
-    public Boolean getUser(String userName) {
+    public boolean getUser(String userName) {
         try {
             FileInputStream file = new FileInputStream(userName);
             ObjectInputStream object = new ObjectInputStream(file);
-            model = new User((User)object.readObject());
+            model = new User((User) object.readObject());
             file.close();
             object.close();
-            return Boolean.TRUE;
+            return true;
         } catch (IOException | ClassNotFoundException e) {
-            return Boolean.FALSE;
+            return false;
         }
     }
 
-    public Boolean userExists() {
+    public boolean deleteUser() {
         File file = new File(model.getUserName());
-        return (file.exists() == Boolean.TRUE && file.isDirectory() == Boolean.FALSE);
+        model = null;
+        return file.delete();
+    }
+
+    public boolean userExists() {
+        File file = new File(model.getUserName());
+        return (file.exists() && !file.isDirectory());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,39 @@
 public class Main {
+
+    private static void testUser() {
+        User user = new User("ilisay", "123456qwerty", "123456qwerty");
+        Controller ctrl = Controller.getInstance(user);
+        System.out.println("User is not created yet");
+        if (ctrl.userExists()) {
+            System.out.println("User " + user.getUserName() + " exists");
+        } else {
+            System.out.println("User " + user.getUserName() + " doesn't exist");
+        }
+        if (ctrl.saveUser()) {
+            System.out.println("User was created");
+        } else {
+            System.out.println("FAILED to create user");
+            return;
+        }
+        if (ctrl.userExists()) {
+            System.out.println("User " + user.getUserName() + " exists");
+        } else {
+            System.out.println("User " + user.getUserName() + " doesn't exist");
+        }
+        if (ctrl.deleteUser()) {
+            System.out.println("User was removed");
+        } else {
+            System.out.println("FAILED to remove user");
+            return;
+        }
+        if (ctrl.userExists()) {
+            System.out.println("User " + user.getUserName() + " exists");
+        } else {
+            System.out.println("User " + user.getUserName() + " doesn't exist");
+        }
+    }
+
     public static void main(String[] args) {
+        testUser();
     }
 }
